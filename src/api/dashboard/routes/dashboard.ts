cd68735dcb1b4af5310a686a364d4cf32bd5453b
muzammil-majeed-viabletree <muzammil@viabletree>
2025-11-21 00:29:32 +0500

    feat: toast notification added

--- src/api/dashboard/routes/dashboard.ts
+++ src/api/dashboard/routes/dashboard.ts
@@ -4,27 +4,18 @@ export default {
       method: "GET",
       path: "/dashboard/summary",
       handler: "dashboard.summary",
-      config: {
-        auth: false,
-      },
     },
 
     {
       method: "GET",
       path: "/dashboard/generateProjectPDF",
       handler: "dashboard.generateProjectPDF",
-      config: {
-        auth: false,
-      },
     },
 
     {
       method: "POST",
       path: "/dashboard/upload-csv",
       handler: "dashboard.uploadCsv",
-      config: {
-        auth: false,
-      },
     },
   ],
 };
